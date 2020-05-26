983ec769d43f16dbe97c7b1d02bbf7c96cc0a577
Karolina <karolina.szlenk@gmail.com>
2020-05-26 20:25:29 +0200

    app structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Users from "./pages/Users";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/users" component={Users}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
