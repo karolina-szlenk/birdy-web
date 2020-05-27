3b0f69ca2c2310bab3093f40fc1cbf1601c0decf
Karolina <karolina.szlenk@gmail.com>
2020-05-27 17:44:21 +0200

    semantic downloaded, nav and contact page updated

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu, Segment } from "semantic-ui-react";
 
 export default function Navigation() {
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <React.Fragment>
+      <Segment inverted >
+        <Menu inverted pointing secondary >
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="home"
+            active={activeItem === "home"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/contact"
+            name="contact"
+            active={activeItem === "contact"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to="/users"
+            name="users"
+            active={activeItem === "users"}
+            onClick={handleItemClick}
+          />
+        </Menu>
+      </Segment>
+      {/* <Link to="/">Home</Link>
       <Link to="/contact">Contact</Link>
-      <Link to="/users">Users</Link>
-    </div>
+      <Link to="/users">Users</Link> */}
+    </React.Fragment>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Form, TextArea, Modal, Button, Header } from "semantic-ui-react";
+import "./Contact.css"
 
 export default function Contact() {
-  return (<div>Contact</div>);
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState("false");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues({ name: "", email: "", subject: "", message: "" });
+  };
+
+  const submitBtn =
+    values.name !== "" &&
+    values.email !== "" &&
+    values.subject !== "" &&
+    values.message !== "" ? (
+      <Form.Button content="Submit" />
+    ) : (
+      <Form.Button content="Submit" disabled />
+    );
+
+  return (
+    <div>
+      <Card className="card-container" centered>
+        <Card.Content>
+          <Card.Header className="title">Contact Form</Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              placeholder="Name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              placeholder="Email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              placeholder="Subject"
+              name="subject"
+              value={values.subject}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              control={TextArea}
+              placeholder="Message"
+              name="message"
+              value={values.message}
+              onChange={handleChange}
+              required
+            />
+            <Modal trigger={submitBtn} closeIcon>
+              <Header icon="check circle outline" content="Thank you for contacting us!"/>
+              <Modal.Content >
+                <p>We appreciate that you've taken the time to write us. We'll get back to you very soon!</p>
+              </Modal.Content>
+            </Modal>
+          </Form>
+        </Card.Content>
+      </Card>
+    </div>
+  );
 }
