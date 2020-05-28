698360898e9f454a214216b86f3d2ef903e73d7f
Karolina <karolina.szlenk@gmail.com>
2020-05-28 06:58:43 +0200

    strict.mode removed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'semantic-ui-css/semantic.min.css'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
