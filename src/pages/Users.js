001d8668bf17d1d46db3ca779169d07b9b4dc7c3
Karolina <karolina.szlenk@gmail.com>
2020-05-31 17:12:50 +0200

    Users structure changed

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -22,47 +22,42 @@ export default function Users() {
           <p>File not found!</p>
         </Message>
       ) : (
-        <Grid textAlign="center" className="page-container">
-          <Grid.Row>
-            <Card>
-              <Card.Content>
-                <Card.Header className="title">App users</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Row>
-          <Grid.Row>
-            {users.map((user) => (
-              <Grid.Column
-                key={user.id}
-                mobile={15}
-                tablet={7}
-                computer={5}
-              >
-                <Card className="card-wrapper" centered>
-                  <Image
-                    src={user.avatar}
-                    wrapped
-                    ui={false}
-                    className="user-image"
-                  />
-                  <Card.Content>
-                    <Card.Header className="subtitle">
-                      {user.first_name}
-                    </Card.Header>
-                    <Button
-                      as={Link}
-                      to={`/users/${user.id}`}
-                      labelPosition="right"
-                      icon="angle double right"
-                      content="More details"
-                      color="blue"
-                    ></Button>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-            ))}
-          </Grid.Row>
-        </Grid>
+        <div className="app-container">
+          <Card className="card-container" centered>
+            <Card.Content>
+              <Card.Header className="title">App users</Card.Header>
+            </Card.Content>
+          </Card>
+          <Grid textAlign="center" className="page-container">
+            <Grid.Row>
+              {users.map((user) => (
+                <Grid.Column key={user.id} mobile={15} tablet={7} computer={5}>
+                  <Card className="card-wrapper" centered>
+                    <Image
+                      src={user.avatar}
+                      wrapped
+                      ui={false}
+                      className="user-image"
+                    />
+                    <Card.Content>
+                      <Card.Header className="subtitle">
+                        {user.first_name}
+                      </Card.Header>
+                      <Button
+                        as={Link}
+                        to={`/users/${user.id}`}
+                        labelPosition="right"
+                        icon="angle double right"
+                        content="More details"
+                        color="blue"
+                      ></Button>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+              ))}
+            </Grid.Row>
+          </Grid>
+        </div>
       )}
     </React.Fragment>
   );
